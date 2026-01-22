3fc7e6b31e6eec6e9a92b69224db40f0bd3fc5f4
Pawel Trajdos <pawel.trajdos@pwr.edu.pl>
2026-01-22 21:19:23 +0100

    1. channel_knn_weights_ex_3 -- weights have been added.

--- dexterous_bioprosthesis_2021_raw_datasets_framework_experiments/channel_knn_weights_ex_3.py
+++ dexterous_bioprosthesis_2021_raw_datasets_framework_experiments/channel_knn_weights_ex_3.py
@@ -442,6 +442,7 @@ def generate_d_nb_soft(
 ):
     pipeline = Pipeline(
         [
+            ("scaler", RobustScaler()),
             (
                 "estimator",
                 AttributeWeightEstimNB(
