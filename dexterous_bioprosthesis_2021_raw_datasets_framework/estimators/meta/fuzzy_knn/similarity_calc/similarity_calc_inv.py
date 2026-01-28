57f3ecc30af98a03179a85586df84a18dad50881
Pawel Trajdos <pawel.trajdos@pwr.edu.pl>
2026-01-28 15:51:40 +0100

    1. similarity_calc_inv -- bugfix.

--- dexterous_bioprosthesis_2021_raw_datasets_framework/estimators/meta/fuzzy_knn/similarity_calc/similarity_calc_inv.py
+++ dexterous_bioprosthesis_2021_raw_datasets_framework/estimators/meta/fuzzy_knn/similarity_calc/similarity_calc_inv.py
@@ -9,6 +9,27 @@ class SimilarityCalcInv(ASimilarityCalc):
     Class for calculating similarity using an inverse distance-related weights.
     """
 
+    def __init__(
+        self, pairwise_distances_func=None, pairwise_distances_kwargs=None
+    ):
+        """
+        Initialize the SimilarityCalcInv with a distance function and its parameters.
+
+        Parameters
+        ----------
+        pairwise_distances_func : callable, optional
+            A function to compute pairwise distances. If None, a default function is used.
+        pairwise_distances_kwargs : dict, optional
+            Additional keyword arguments for the distance function.
+        """
+        super().__init__(pairwise_distances_func, pairwise_distances_kwargs)
+
+    def fit(self, X):
+        super().fit(X)
+        self.pairwise_distances_func_ = self._get_effective_pairwise_distances_func()
+        self.pairwise_distances_kwargs_ = (
+            self._get_effective_pairwise_distances_kwargs()
+        )
 
     def pairwise_similarity(self, X, Y=None):
         """
